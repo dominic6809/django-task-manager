f56e8680d67efa9bc7069ac5fd57e78dfa541cdc
dominic6809 <muuomusyoki018@gmail.com>
2024-09-05 11:55:29 +0300

    update

--- tasks/static/tasks/js/script.js
+++ tasks/static/tasks/js/script.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Select all alerts
     var alerts = document.querySelectorAll('.alert');
     alerts.forEach(function(alert) {
-        // Set a timeout to hide each alert after 3 seconds.
+        // Set timeout to hide each alert after 3 seconds.
         setTimeout(function() {
             alert.classList.remove('show');
             alert.classList.add('fade');
